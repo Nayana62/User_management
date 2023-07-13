9010cf1cefb3f4113c638c51ada2233f9c5935eb
Nayana N M <95485122+Nayana62@users.noreply.github.com>
2023-07-13 20:02:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import UserRoute from "./Routes/userRoute.js";
 const app = express();
 app.use(cors());
 dotenv.config();
-app.use(express.json());
+
 
 mongoose
   .connect("mongodb+srv://nmnayana62:HWLnWrGsCtQUBkCj@cluster0.6gcu7g8.mongodb.net/User_Management?retryWrites=true&w=majority", {
