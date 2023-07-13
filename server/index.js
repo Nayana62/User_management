a92ce8e529d37b02353b8c0dade34e75bedf671c
Nayana N M <95485122+Nayana62@users.noreply.github.com>
2023-07-13 20:09:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
 import express from "express";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import cors from "cors";
 import UserRoute from "./Routes/userRoute.js";
 
 const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-dotenv.config();
 
 mongoose
   .connect("mongodb+srv://nmnayana62:HWLnWrGsCtQUBkCj@cluster0.6gcu7g8.mongodb.net/User_Management?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  
 
 app.use("/users", UserRoute);
