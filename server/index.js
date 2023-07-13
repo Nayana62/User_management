28b3c0cdb8280c62c1a59c08fe954717a3f99ab8
Nayana N M <95485122+Nayana62@users.noreply.github.com>
2023-07-13 19:53:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,10 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import UserRoute from "./Routes/userRoute.js";
 
 const app = express();
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
   {
     origin: [https://user-management-frontend-tau.vercel.app],
@@ -17,9 +13,10 @@ app.use(cors(
   }
 ));
 dotenv.config();
+app.use(express.json());
 
 mongoose
-  .connect(mongodb+srv://nmnayana62:HWLnWrGsCtQUBkCj@cluster0.6gcu7g8.mongodb.net/User_Management?retryWrites=true&w=majority, {
+  .connect("mongodb+srv://nmnayana62:HWLnWrGsCtQUBkCj@cluster0.6gcu7g8.mongodb.net/User_Management?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
